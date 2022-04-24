a227edb5d216cfe69ef40522689d961e18115fc2
Andreas Ambühl <36681000+AndiSwiss@users.noreply.github.com>
2022-04-24 18:23:52 +0200

    FEAT: introduce shared resources

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -2,7 +2,7 @@ const Header = (props) => <h1>{props.title}</h1>
 
 const Part = (props) => <p>{props.part.name} {props.part.exercises}</p>
 
-const Content = (props) => <>{props.parts.map(p => <Part part={p}/>)}</>
+const Content = (props) => <>{props.parts.map((p, index) => <Part key={index} part={p}/>)}</>
 
 const Total = (props) => <p>Number of exercises {props.parts.map(p => p.exercises).reduce((a, b) => a + b, 0)}</p>
 
