ff06abc301111292e629f0f088c2d63743992eca
Andreas Ambühl <36681000+AndiSwiss@users.noreply.github.com>
2022-04-24 11:05:11 +0200

    REFACTOR: done until exercise 1.5

---
+++ part1/lesson-code/playground1.js
@@ -0,0 +1,8 @@
+// Stuff from https://fullstackopen.com/en/part1/java_script
+// Run with  node playground1.js
+
+const t = [1, 2, 3, 4, 5]
+const [first, second] = t
+
+console.log(`first = `, first)
+console.log(`second = `, second)
