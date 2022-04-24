6015540d50f760dc655a84663992aadb0540604d
Andreas Ambühl <36681000+AndiSwiss@users.noreply.github.com>
2022-04-24 11:05:11 +0200

    REFACTOR: done until exercise 1.5

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -2,33 +2,33 @@ const Header = (props) => <h1>{props.title}</h1>
 
 const Part = (props) => <p>{props.part.name} {props.part.exercises}</p>
 
-const Content = (props) => (<>
-  <Part part={props.part1}/>
-  <Part part={props.part2}/>
-  <Part part={props.part3}/>
-</>)
+const Content = (props) => <>{props.parts.map(p => <Part part={p}/>)}</>
 
-const Total = (props) => <p>Number of exercises {props.total}</p>
+const Total = (props) => <p>Number of exercises {props.parts.map(p => p.exercises).reduce((a, b) => a + b, 0)}</p>
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
   }
 
   return (<>
-    <Header title={course}/>
-    <Content part1={part1} part2={part2} part3={part3}/>
-    <Total total={part1.exercises + part2.exercises + part3.exercises}/>
+    <Header title={course.name}/>
+    <Content parts={course.parts}/>
+    <Total parts={course.parts}/>
   </>)
 }
 
