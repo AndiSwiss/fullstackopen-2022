67e39aa3a75b5d581ba683ca12b3153f0eecaba4
Andreas Ambühl <36681000+AndiSwiss@users.noreply.github.com>
2022-05-08 21:37:11 +0200

    FEATURE: implement adding notes

--- part2/lesson-code/src/App.js
+++ part2/lesson-code/src/App.js
@@ -3,8 +3,31 @@
  */
 
 import Note from "./components/Note";
+import {useState} from "react";
 
-const App = ({notes}) => {
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes)
+  const [newNote, setNewNote] = useState('a new note')
+
+  const addNote = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() < 0.5,
+      id: notes.length + 1, // only works, if notes are never deleted!
+    }
+
+    setNotes(notes.concat(noteObject))
+    setNewNote('')
+  }
+
+  const handleNoteChange = (event) => {
+    console.log(`event.target.value = `, event.target.value)
+    setNewNote(event.target.value)
+  }
+
+
   return (<>
     <h1>Notes</h1>
     <ul>
@@ -12,6 +35,10 @@ const App = ({notes}) => {
         <Note key={note.id} note={note}/>
       )}
     </ul>
+    <form onSubmit={addNote}>
+      <input value={newNote} onChange={handleNoteChange}/>
+      <button type="submit">save</button>
+    </form>
   </>)
 }
 
