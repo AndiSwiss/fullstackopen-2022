951eec11ebe337aa6575e8fa41dee96c40eb62cd
Andreas Ambühl <36681000+AndiSwiss@users.noreply.github.com>
2022-05-08 23:32:33 +0200

    FEATURE: finish exercise 2.10

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,11 +1,14 @@
 import {useState} from "react";
+import Filter from "./components/Filter";
+import PersonForm from "./components/PersonForm";
+import Persons from "./components/Persons";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    {name: 'Arto Hellas', number: '040-123456', id: 1},
-    {name: 'Ada Lovelace', number: '39-44-5323523', id: 2},
-    {name: 'Dan Abramov', number: '12-43-234345', id: 3},
-    {name: 'Mary Poppendieck', number: '39-23-6423122', id: 4}
+    {name: 'Arto Hellas', number: '040-123456'},
+    {name: 'Ada Lovelace', number: '39-44-5323523'},
+    {name: 'Dan Abramov', number: '12-43-234345'},
+    {name: 'Mary Poppendieck', number: '39-23-6423122'}
   ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
@@ -48,22 +51,17 @@ const App = () => {
 
   return (<>
     <h1>Phonebook</h1>
-    <div>filter shown with <input value={filter} onChange={handleFilterChange}/></div>
+    <Filter filter={filter} handleFilterChange={handleFilterChange}/>
     <h2>Add new</h2>
-    <form onSubmit={addPerson}>
-      <div>name: <input value={newName} onChange={handleNameChange}/></div>
-      <div>number: <input value={newNumber} onChange={handleNumberChange}/></div>
-      <div>
-        <button type="submit">add</button>
-      </div>
-    </form>
-
+    <PersonForm
+      addPerson={addPerson}
+      newName={newName}
+      handleNameChange={handleNameChange}
+      newNumber={newNumber}
+      handleNumberChange={handleNumberChange}
+    />
     <h2>Numbers</h2>
-    {filteredPersons.map(person => <div key={person.name}>{person.name}: {person.number}</div>)}
-
-    {/* TODO: debugging section */}
-    <hr/>
-    <div><em>debug: newName={newName}</em></div>
+    <Persons persons={filteredPersons}/>
   </>)
 }
 
---
+++ part2/phonebook/src/components/Filter.js
@@ -0,0 +1,7 @@
+
+const Filter = ({filter, handleFilterChange}) => (
+  <div>filter shown with <input value={filter} onChange={handleFilterChange} placeholder="search..."/></div>
+)
+
+
+export default Filter
