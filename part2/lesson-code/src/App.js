76e88aceb11000244237635951f390f1ea4bbcf6
Andreas Ambühl <36681000+AndiSwiss@users.noreply.github.com>
2022-05-08 21:51:41 +0200

    FEATURE: trying out lesson code

--- part2/lesson-code/src/App.js
+++ part2/lesson-code/src/App.js
@@ -7,7 +7,8 @@ import {useState} from "react";
 
 const App = (props) => {
   const [notes, setNotes] = useState(props.notes)
-  const [newNote, setNewNote] = useState('a new note')
+  const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(true)
 
   const addNote = (event) => {
     event.preventDefault()
@@ -17,26 +18,32 @@ const App = (props) => {
       important: Math.random() < 0.5,
       id: notes.length + 1, // only works, if notes are never deleted!
     }
-
     setNotes(notes.concat(noteObject))
     setNewNote('')
   }
 
   const handleNoteChange = (event) => {
-    console.log(`event.target.value = `, event.target.value)
     setNewNote(event.target.value)
   }
 
+  const notesToShow = showAll
+    ? notes
+    : notes.filter(note => note.important)
 
   return (<>
     <h1>Notes</h1>
+    <div>
+      <button onClick={() => setShowAll(!showAll)}>
+        show {showAll ? 'important' : 'all'}
+      </button>
+    </div>
     <ul>
-      {notes.map(note =>
+      {notesToShow.map(note =>
         <Note key={note.id} note={note}/>
       )}
     </ul>
     <form onSubmit={addNote}>
-      <input value={newNote} onChange={handleNoteChange}/>
+      <input value={newNote} onChange={handleNoteChange} placeholder={'Your new note...'}/>
       <button type="submit">save</button>
     </form>
   </>)
--- part2/lesson-code/src/components/Note.js
+++ part2/lesson-code/src/components/Note.js
@@ -1,3 +1,13 @@
-const Note = ({note}) => <li key={note.id}>{note.content}</li>
+/**
+ * Note: This can be done simpler and cleaner with a CSS-class
+ */
+const MaybeBold = ({content, bold}) => (bold
+    ? <strong>{content}</strong>
+    : content
+)
+
+const Note = ({note}) => <li key={note.id}>
+  <MaybeBold content={note.content} bold={note.important}/>
+</li>
 
 export default Note
