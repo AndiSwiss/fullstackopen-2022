19ed27896de113b0efc3de9e0864ee954cc98718
Andreas Ambühl <36681000+AndiSwiss@users.noreply.github.com>
2022-05-08 22:35:50 +0200

    FEATURE: finish exercise 2.6 and 2.7

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,22 +2,46 @@ import {useState} from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    {name: 'Arto Hellas'}
+    {name: 'Arto Hellas'},
+    {name: 'Ada Lovelace'}
   ])
   const [newName, setNewName] = useState('')
 
+  const addPerson = (event) => {
+    event.preventDefault()
+    // check if name is not already in the phonebook
+    if (persons.find(person => person.name === newName)) {
+      alert(`${newName} is already added to the phonebook`)
+    } else {
+      const personObject = {
+        name: newName,
+      }
+      setPersons(persons.concat(personObject))
+      setNewName('')
+    }
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
   return (<>
     <h2>Phonebook</h2>
-    <form>
+    <form onSubmit={addPerson}>
       <div>
-        name: <input/>
+        name: <input value={newName} onChange={handleNameChange}/>
       </div>
       <div>
         <button type="submit">add</button>
       </div>
     </form>
+
     <h2>Numbers</h2>
-    ...
+    {persons.map(person => <div key={person.name}>{person.name}</div>)}
+
+    {/* TODO: debugging section */}
+    <hr/>
+    <div><em>debug: newName={newName}</em></div>
   </>)
 }
 
