fd45cdd1e8176eaed7999db59b068b27bc14caf5
Andreas Ambühl <36681000+andiswiss@users.noreply.github.com>
2022-05-17 17:32:20 +0200

    FEATURE: Part 2d - finished lesson-code

--- part2/lesson-code/src/services/notes.js
+++ part2/lesson-code/src/services/notes.js
@@ -2,9 +2,23 @@ import axios from "axios";
 
 const baseUrl = 'http://localhost:3001/notes'
 
-const getAll = () => axios
-  .get(baseUrl)
-  .then(response => response.data)
+// const getAll = () => axios
+//   .get(baseUrl)
+//   .then(response => response.data)
+
+
+const getAll = () => {
+  const request = axios.get(baseUrl)
+  // Smuggling a non-existing note
+  // => lets us test the catch-clause in toggleImportanceOf:
+  const nonExisting = {
+    id: 10000,
+    content: 'This note is not saved to server',
+    date: '2019-05-30T17:30:31.098Z',
+    important: true
+  }
+  return request.then(response => response.data.concat(nonExisting))
+}
 
 const create = newObject => axios
   .post(baseUrl, newObject)
