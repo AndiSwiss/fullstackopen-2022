54477a5932781686c1edd3554582c9c1c98e4d03
Andreas Ambühl <36681000+andiswiss@users.noreply.github.com>
2022-05-20 15:59:28 +0200

    FEATURE: finish lesson code for part 2e

--- part2/lesson-code/src/App.js
+++ part2/lesson-code/src/App.js
@@ -5,11 +5,14 @@
 import Note from "./components/Note";
 import {useEffect, useState} from "react";
 import noteService from './services/notes'
+import Notification from "./components/Notification";
+import Footer from "./components/Footer";
 
 const App = () => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage] = useState(null)
 
   const addNote = (event) => {
     event.preventDefault()
@@ -38,14 +41,17 @@ const App = () => {
 
   const toggleImportanceOf = (id) => () => {
     const note = notes.find(n => n.id === id)
-    const changedNote = { ...note, important: !note.important}
+    const changedNote = {...note, important: !note.important}
     noteService
       .update(id, changedNote)
       .then(returnedNote => setNotes(notes.map(note => note.id !== id ? note : returnedNote)))
       .catch(error => {
-        alert(`the note '${note.content}' was already deleted from server!`)
+        setErrorMessage(
+          `The note '${note.content}' was already deleted from server!`
+        )
+        setTimeout(() => setErrorMessage(null), 5000)
+        setNotes(notes.filter(n => n.id !== id))
       })
-    setNotes(notes.filter(n => n.id !== id))
   }
 
   useEffect(() => {
@@ -56,6 +62,7 @@ const App = () => {
 
   return (<>
     <h1>Notes</h1>
+    <Notification message={errorMessage}/>
     <div>
       <button onClick={() => setShowAll(!showAll)}>
         show {showAll ? 'important' : 'all'}
@@ -70,6 +77,7 @@ const App = () => {
       <input value={newNote} onChange={handleNoteChange} placeholder={'Your new note...'}/>
       <button type="submit">save</button>
     </form>
+    <Footer/>
   </>)
 }
 
--- part2/lesson-code/src/components/Note.js
+++ part2/lesson-code/src/components/Note.js
@@ -9,7 +9,7 @@ const MaybeBold = ({content, bold}) => (bold
 const Note = ({note, toggleImportance}) => {
   const label = note.important ? 'make not important' : 'make important'
 
-  return (<li key={note.id}>
+  return (<li className="note" key={note.id}>
     <MaybeBold content={note.content} bold={note.important}/> <button onClick={toggleImportance}>{label}</button>
   </li>)
 }
