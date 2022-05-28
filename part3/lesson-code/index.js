c4791d517f8f785633ee945e78cd0b6b6bfb0725
Andreas Ambühl <36681000+andiswiss@users.noreply.github.com>
2022-05-28 17:31:41 +0200

    delete notes implemented

--- part3/lesson-code/index.js
+++ part3/lesson-code/index.js
@@ -37,6 +37,12 @@ app.get('/api/notes/:id', (request, response) => {
   else response.status(404).end()
 })
 
+app.delete('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  notes = notes.filter(note => note.id !== id)
+  response.status(204).end()
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`)
