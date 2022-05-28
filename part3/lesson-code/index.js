38a53fab53ccbad113f80195be715820f98d97af
Andreas Ambühl <36681000+andiswiss@users.noreply.github.com>
2022-05-28 17:56:48 +0200

    finish lesson code for part3a 'Node.js and Express'

--- part3/lesson-code/index.js
+++ part3/lesson-code/index.js
@@ -24,14 +24,17 @@ let notes = [
   }
 ]
 
+// GET '/' => hello world
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
 
+// GET all notes
 app.get('/api/notes', (request, response) => {
   response.json(notes)
 })
 
+// GET individual note
 app.get('/api/notes/:id', (request, response) => {
   const id = Number(request.params.id) // Don't forget to convert from string to Number
   const note = notes.find(note => note.id === id)
@@ -39,22 +42,39 @@ app.get('/api/notes/:id', (request, response) => {
   else response.status(404).end()
 })
 
-app.post('/api/notes', (request, response) => {
+// Generate a new id
+const generateId = () => {
   const maxId = notes.length > 0
     ? Math.max(...notes.map(n => n.id))
     : 0
-  const note = request.body
-  note.id = maxId + 1
+  return maxId + 1
+}
+
+// POST a new note
+app.post('/api/notes', (request, response) => {
+  const body = request.body
+
+  // Abort if there is no valid body
+  if (!body.content) return response.status(400).json({error: 'content missing (JSON expected)'})
+
+  const note = {
+    id: generateId(),
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+  }
   notes = notes.concat(note)
   response.json(note)
 })
 
+// DELETE a note
 app.delete('/api/notes/:id', (request, response) => {
   const id = Number(request.params.id)
   notes = notes.filter(note => note.id !== id)
   response.status(204).end()
 })
 
+// Run app
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`)
