15aee4e1340a378fc96ee943abdee41cf84b418a
Andreas Ambühl <36681000+andiswiss@users.noreply.github.com>
2022-08-13 14:43:00 +0200

    database works

--- part4/part4a-exercises/index.js
+++ part4/part4a-exercises/index.js
@@ -1,9 +1,9 @@
+require('dotenv').config()
 const http = require('http')
 const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
-const { response } = require('express')
 
 const blogSchema = new mongoose.Schema({
   title: String,
@@ -14,9 +14,15 @@ const blogSchema = new mongoose.Schema({
 
 const Blog = mongoose.model('Blog', blogSchema)
 
-// TODO: check!!
-const mongoUrl = 'mongodb://localhost/bloglist'
-mongoose.connect(mongoUrl)
+// from https://cloud.mongodb.com => Button 'Connect' => 'Connect your application'
+const MONGODB_URL = process.env.MONGODB_URL
+if (!MONGODB_URL) throw 'MONGODB_URL is missing in .env!'
+mongoose.connect(MONGODB_URL)
+  .then(() => console.log(`connected to MongoDB: ${MONGODB_URL}`))
+  .catch(error => {
+    console.log(`error connecting to MongoDB: ${MONGODB_URL}`)
+    console.log(`error-message: ${error}`)
+  })
 
 app.use(cors())
 app.use(express.json())
@@ -41,5 +47,5 @@ app.post('/api/blogs', (request, response) => {
 
 const PORT = 3003
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on http://localhost:${PORT}`)
 })
