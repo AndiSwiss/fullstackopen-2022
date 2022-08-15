86213309d2248a771dcd540508808dbff71355c9
Andreas Ambühl <36681000+andiswiss@users.noreply.github.com>
2022-08-15 09:42:35 +0200

    exercise 4.2 refactor controller

--- part4/part4a-exercises/app.js
+++ part4/part4a-exercises/app.js
@@ -4,7 +4,7 @@ const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 const { info } = require('./utils/logger')
-const Blog = require('./models/blog')
+const blogRouter = require('./controllers/blogs')
 
 
 // from https://cloud.mongodb.com => Button 'Connect' => 'Connect your application'
@@ -20,22 +20,6 @@ mongoose.connect(MONGODB_URL)
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use('/api/blogs', blogRouter) // Use all the routes for the blog api
 
 module.exports = app
