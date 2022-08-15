c64a2e0dfac9d3574a19debc85bdc82a6990e9da
Andreas Ambühl <36681000+andiswiss@users.noreply.github.com>
2022-08-15 09:52:23 +0200

    exercise 4.2 add middleware

--- part4/part4a-exercises/app.js
+++ part4/part4a-exercises/app.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose')
 const { info } = require('./utils/logger')
 const blogRouter = require('./controllers/blogs')
 const { MONGODB_URL, MONGODB_URL_NO_PW } = require('./utils/config')
+const middleware = require('./utils/middleware')
 
 // from https://cloud.mongodb.com => Button 'Connect' => 'Connect your application'
 mongoose.connect(MONGODB_URL)
@@ -17,7 +18,9 @@ mongoose.connect(MONGODB_URL)
 
 app.use(cors())
 app.use(express.json())
-
+app.use(middleware.requestLogger)
 app.use('/api/blogs', blogRouter) // Use all the routes for the blog api
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
