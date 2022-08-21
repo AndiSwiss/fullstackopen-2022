80f2f56dbde41cccb1b9d4ccc27624eb7a71fd03
Andreas Ambühl <36681000+andiswiss@users.noreply.github.com>
2022-08-21 14:37:12 +0200

    first tests implemented

---
+++ part4/part4a-exercises/utils/for_testing.js
@@ -0,0 +1,12 @@
+/**
+ * From
+ * https://fullstackopen.com/en/part4/structure_of_backend_application_introduction_to_testing#testing-node-applications
+ */
+
+const reverse = str => str.split('').reverse().join('')
+
+const reducer = (sum, item) => sum + item
+
+const average = array => array.reduce(reducer, 0) / array.length
+
+module.exports = { reverse, average }
