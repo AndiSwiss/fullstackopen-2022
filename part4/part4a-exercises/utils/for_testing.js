f0184b1ebfe70dd47ac9a08ef0920187c780cd3a
Andreas Ambühl <36681000+andiswiss@users.noreply.github.com>
2022-08-21 14:45:47 +0200

    added more tests

--- part4/part4a-exercises/utils/for_testing.js
+++ part4/part4a-exercises/utils/for_testing.js
@@ -7,6 +7,8 @@ const reverse = str => str.split('').reverse().join('')
 
 const reducer = (sum, item) => sum + item
 
-const average = array => array.reduce(reducer, 0) / array.length
+const average = array => array.length === 0
+  ? 0
+  : array.reduce(reducer, 0) / array.length
 
 module.exports = { reverse, average }
