c8bdc897eb7dd4cbe92593397d9e1feb751ab5c8
Andreas Ambühl <36681000+andiswiss@users.noreply.github.com>
2022-08-29 14:00:14 +0200

    part4b exercise 4.8 finished

---
+++ part4/part4b-exercises/tests/blog_api.test.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const api = supertest(app)
+const Blog = require('../models/blog')
+const helper = require('./test_helper')
+
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  for (const blog of helper.initialBlogs) {
+    const blogObject = new Blog(blog)
+    await blogObject.save()
+  }
+
+  //Other variant (also works) => https://fullstackopen.com/en/part4/testing_the_backend#optimizing-the-before-each-function
+  // const blogObjects = helper.initialBlogs.map(blog => new Blog(blog))
+  // const promiseArray = blogObjects.map(blog => blog.save())
+  // await Promise.all(promiseArray)
+})
+
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+afterAll(() => {
+  mongoose.connection.close()
+})
---
+++ part4/part4b-exercises/tests/test_helper.js
@@ -0,0 +1,61 @@
+const Blog = require('../models/blog')
+
+const initialBlogs = [
+  {
+    title: "something",
+    author: "me",
+    url: "google.com",
+    likes: 4,
+  },
+  {
+    title: "smart stuff",
+    author: "hugo",
+    url: "",
+    likes: 5,
+  },
+  {
+    title: "another one",
+    author: "me",
+    url: "yahoo.com",
+    likes: 3,
+  },
+]
+
+const aBlog = new Blog({ content: 'willremovethissoon', date: new Date() })
+
+const nonExistingId = async () => {
+  await aBlog.save()
+  await aBlog.remove()
+  return aBlog._id.toString()
+}
+
+const blogsInDb = async () => {
+  const blogs = await Blog.find({})
+  /* NOTE:
+   * The following changes:
+   *    _id: new ObjectId("630b8efa126f08d6cecc0f79")    ==>     id: '630b8efa126f08d6cecc0f79'
+   * And removes the    __v: 0
+   *
+   * BEFORE:
+   * =======
+     {
+        _id: new ObjectId("630b8efa126f08d6cecc0f79"),
+        content: 'HTML is easy',
+        date: 2022-08-28T15:51:21.839Z,
+        important: false,
+        __v: 0
+      }
+   *
+   * AFTER:
+   * ======
+     {
+        content: 'HTML is easy',
+        date: 2022-08-28T15:51:21.839Z,
+        important: false,
+        id: '630b8efa126f08d6cecc0f79'
+      }
+   */
+  return blogs.map(blog => blog.toJSON())
+}
+
+module.exports = { initialBlogs, nonExistingId, blogsInDb }
